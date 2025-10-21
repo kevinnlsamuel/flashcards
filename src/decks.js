3caf9e7a33c0d16a69868967c9b6045e3c3276d0
kevinnls <developer@kevinnlsamuel.com>
2025-10-22 02:51:30 +0530

    show on list

--- src/decks.js
+++ src/decks.js
@@ -1,1 +1,9 @@
-	const decks = ["deck.fr.min.json","deck.min.json","gr1.002-vocab.min.json","gr1.003-nums.min.json","gr2.002-vocab.min.json","gr2.003-nums.min.json"]; export { decks }
+const decks =
+	[
+		"inspire1-unite1.min.json",
+	"deck.fr.min.json",
+	"deck.min.json",
+	 "gr1.002-vocab.min.json",
+	 "gr1.003-nums.min.json",
+	 "gr2.002-vocab.min.json",
+	 "gr2.003-nums.min.json"]; export { decks }
