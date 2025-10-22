d8d4e34a4225c63144d38f44d89b80a071a15e99
kevinnls <developer@kevinnlsamuel.com>
2025-10-22 10:18:45 +0530

    i cannot no more

--- src/decks.js
+++ src/decks.js
@@ -1,9 +1,1 @@
-const decks =
-	[
-		"inspire1-unite1.min.json",
-	"deck.fr.min.json",
-	"deck.min.json",
-	 "gr1.002-vocab.min.json",
-	 "gr1.003-nums.min.json",
-	 "gr2.002-vocab.min.json",
-	 "gr2.003-nums.min.json"]; export { decks }
+	const decks = ["deck.fr.min.json","deck.min.json","gr1.002-vocab.min.json","gr1.003-nums.min.json","gr2.002-vocab.min.json","gr2.003-nums.min.json","inspire1-unite1-demo.min.json","inspire1-unite1.min.json"]; export { decks }
